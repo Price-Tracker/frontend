5427ea8ee3da9e8bb73f43a16056963db82d092a
Neosqweuzzer <85544252+Neosqweuzzer@users.noreply.github.com>
2023-05-11 21:40:58 +0300

    Main page: update

---
+++ src/assets/js/slider.js
@@ -0,0 +1,71 @@
+let slideIndex = 1;
+let timer;
+
+showSlides(slideIndex);
+
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
+
+function previousSlide() {
+    showSlides(slideIndex -= 1);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("item");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+showSlides(slideIndex);
+
+function nextSlide() {
+    showSlides(slideIndex += 1);
+    resetTimer();
+}
+
+function previousSlide() {
+    showSlides(slideIndex -= 1);
+    resetTimer();
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+    resetTimer();
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("item");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+function startTimer() {
+    timer = setInterval(nextSlide, 10000);
+}
+
+function resetTimer() {
+    clearInterval(timer);
+    startTimer();
+}
+
+startTimer(); 
