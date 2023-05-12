6f39baaa3cb20e5aea556df7046d3542feff4944
ElXreno <elxreno@gmail.com>
2023-05-12 20:00:30 +0300

    Import minimal flake with nodejs

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          nativeBuildInputs = [
+            nodejs
+            libwebp
+          ];
+        };
+      });
+}
