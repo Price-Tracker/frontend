9bf9eac8fe321808df93c6d38fb51abef081366b
ElXreno <elxreno@gmail.com>
2023-05-13 20:23:35 +0300

    Fun with index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,35 @@
 <template>
-    <br>
-    <b>Hello!</b>
+    <div class="flex flex-wrap justify-center">
+        <div v-for="(item, index) in productNames"
+            class="m-5 border border-blue-300 shadow rounded-md p-4 max-w-2xl w-full">
+            <div class="animate-pulse flex space-x-4">
+                <div class="rounded-full bg-slate-200 h-20 w-20"></div>
+                <div class="flex-1 space-y-6 py-1">
+                    <div class="h-2 bg-slate-200 rounded"></div>
+                    <div class="space-y-3">
+                        <div class="grid grid-cols-3 gap-4">
+                            <div class="h-2 bg-slate-200 rounded col-span-2"></div>
+                            <div class="h-2 bg-slate-200 rounded col-span-1"></div>
+                        </div>
+                        <div class="h-2 bg-slate-200 rounded"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            productNames: [
+                { name: 'Товар 1' },
+                { name: 'Товар 2' },
+                { name: 'Товар 3' },
+                { name: 'Товар 4' },
+            ],
+        };
+    },
+};
+</script>
