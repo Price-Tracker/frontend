ad3c9279cd624916d31e6060e3af02742dc3d9ea
ElXreno <elxreno@gmail.com>
2023-05-13 21:21:18 +0300

    Change footer text color

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
-    <footer class="bg-black">
+    <footer class="bg-black text-gray-300">
         <div class="container mx-auto px-4 py-4">
             <div class="flex justify-around">
                 <div class="grid">
-                    <div class="font-bold text-lg mb-2 text-white">
+                    <div class="font-bold text-lg mb-2">
                         <p>О сервисе</p>
                         <p>Возможности</p>
                         <p>Применение</p>
@@ -13,7 +13,7 @@
                 </div>
 
                 <div class="grid">
-                    <div class="font-bold text-lg mb-2 text-white">
+                    <div class="font-bold text-lg mb-2">
                         <p>База знаний</p>
                         <p>Настройка системы</p>
                         <p>Справочный центр</p>
