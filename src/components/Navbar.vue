0774157a2b0f640dd6aad329db0c70e4b0239ae2
ElXreno <elxreno@gmail.com>
2023-05-15 02:54:33 +0300

    Beautify navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="bg-black">
         <div class="container mx-auto px-4 py-4">
-            <div class="flex justify-around items-center">
+            <div class="grid grid-cols-3 justify-items-center">
                 <nav class="flex justify-around items-center">
                     <ul class="flex space-x-4">
                         <li v-for="link of links_part1">
@@ -11,24 +11,19 @@
                         </li>
                     </ul>
                 </nav>
-                <div class="flex items-center bg-gray-100 rounded-full px-4 py-2">
+                <div class="flex items-center bg-gray-100 rounded-full px-4 py-2 m-2">
                     <input class="bg-transparent outline-none w-full text-gray-700 leading-tight focus:outline-none"
-                        type="text" placeholder="Search...">
+                        type="text" placeholder="Поиск">
                     <button class="flex-shrink-0 text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700"
                         type="button">
-                        <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                            <path
-                                d="M9 0C4.582 0 1 3.582 1 8s3.582 8 8 8 8-3.582 8-8-3.582-8-8-8zm0 14.667c-3.158 0-5.667-2.51-5.667-5.667S5.842 3.333 9 3.333s5.667 2.51 5.667 5.667-2.51 5.667-5.667 5.667z" />
-                            <path
-                                d="M19.707 18.293l-4.8-4.8a7.971 7.971 0 0 0 1.6-4.8c0-4.418-3.582-8-8-8S0 3.582 0 8s3.582 8 8 8a7.971 7.971 0 0 0 4.8-1.6l4.8 4.8a1.061 1.061 0 0 0 1.5 0c.4-.4.4-1.066 0-1.466zM2 8c0-3.309 2.691-6 6-6s6 2.691 6 6-2.691 6-6 6-6-2.691-6-6z" />
-                        </svg>
+                        <Icon name="ph:magnifying-glass" size="1.5em"></Icon>
                     </button>
                 </div>
-                <nav>
-                    <ul class="flex items-center space-x-4">
+                <nav class="flex items-center leading-5">
+                    <ul class="flex space-x-4">
                         <li>
                             <button class="flex flex-col items-center text-white hover:text-gray-300">
-                                <Icon name="ph:user" size="2em" />
+                                <Icon name="ph:user" size="1.5em" />
                                 <NuxtLink v-if="isLoggedIn" to="/account">
                                     {{ username }}
                                 </NuxtLink>
@@ -39,7 +34,7 @@
                         </li>
                         <li v-for="link of links_part3">
                             <button class="flex flex-col items-center text-white hover:text-gray-300">
-                                <Icon :name="link.icon" size="2em" />
+                                <Icon :name="link.icon" size="1.5em" />
                                 <NuxtLink :to="link.to">
                                     {{ link.name }}
                                 </NuxtLink>
