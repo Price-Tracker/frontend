d98753a9e45bc1415a0ba1124466b451d11510a0
Neosqweuzzer <85544252+Neosqweuzzer@users.noreply.github.com>
2023-05-16 02:42:58 +0300

    Product update

--- src/components/product/Item.vue
+++ src/components/product/Item.vue
@@ -1,19 +1,15 @@
 <template>
-    <div class="relative pt-4">
-        <img :src="product.product.picture_url" class="w-full">
-        <Icon class="absolute top-0 right-0 hover:text-pink-600 transition-colors duration-200"
-            name="ph:heart-straight-light" size="2em" />
-        <Icon class="absolute top-0 right-0 hover:text-pink-600 transition-colors duration-200" name="ph:heart-fill"
-            size="2em" />
+    <div class="relative pt-4 pb-4 bg-gray-100">
+        <img :src="product.product.picture_url" class="h-full w-full">
+        <Icon class="absolute top-0 right-0 hover:text-pink-600 transition-colors duration-200" name="ph:heart-straight-fill" size="2em" />
+        <!-- <Icon class="absolute top-0 right-0 hover:text-pink-600 transition-colors duration-200" name="ph:heart-straight-light" size="2em" /> -->
     </div>
-    <div class="p-4 mt-4 flex justify-between">
-        <div>
-            <p class="mt-1 text-xl text-black">{{ product.product.name }}</p>
-        </div>
-        <p v-if="product.min_price === product.max_price" class="whitespace-nowrap mt-1 font-semibold text-center text-base text-black">
+    <div class="p-4 mt-4">
+            <p class="mt-1 text-xl text-black md:w-auto">{{ product.product.name }}</p>
+        <p v-if="product.min_price === product.max_price" class="whitespace-nowrap mt-8 font-bold text-center text-base text-black">
             {{ product.min_price }} р.
         </p>
-        <p v-else class="mt-1 font-semibold text-center text-base text-black">
+        <p v-else class="mt-8 font-bold text-center text-base text-black">
             {{ product.min_price }} — {{ product.max_price }} р.
         </p>
     </div>
--- src/components/product/List.vue
+++ src/components/product/List.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bg-white">
         <div class="container mx-auto px-4 py-8 sm:px-6 lg:px-8">
-            <p class="px-4 py-8 text-4xl  text-center font-bold text-gray-900 dark:text-white">Хит продаж!</p>
+            <p class="px-4 py-8 text-4xl text-center font-bold text-gray-900 dark:text-white">Хит продаж!</p>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 <div v-for="product in products" class="bg-white shadow-md overflow-hidden">
                     <ProductItem :product="product" />
