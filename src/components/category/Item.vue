c40390f30d322b070ad6137a024df372aebab569
ElXreno <elxreno@gmail.com>
2023-05-15 04:41:29 +0300

    Remove category description

--- src/components/category/Item.vue
+++ src/components/category/Item.vue
@@ -2,7 +2,6 @@
     <img class="h-48 w-full object-cover" src="https://via.placeholder.com/350x200" alt="Category Image">
     <div class="p-4">
         <h3 class="text-lg font-medium text-gray-900">{{ category.name }}</h3>
-        <p class="text-gray-700">category_description</p>
     </div>
 </template>
 
