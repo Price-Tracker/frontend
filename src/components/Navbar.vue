f539b47dacd000c77fa036bb3ced11e43fabe94f
ElXreno <elxreno@gmail.com>
2023-05-15 20:00:12 +0300

    Navbar changes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="bg-white border-b border-gray-200">
         <div class="container mx-auto px-4 py-4">
-            <div class="grid grid-cols-3 justify-items-center">
+            <div class="grid grid-cols-3 grid-flow-col justify-items-center">
                 <nav class="flex justify-around items-center">
                     <ul class="flex space-x-4">
                         <li v-for="link of links_part1">
@@ -11,12 +11,12 @@
                         </li>
                     </ul>
                 </nav>
-                <div class="flex items-center border border-black bg-white rounded-full px-4 py-2 m-2 w-96">
+                <div class="flex items-center border bg-gray-100 rounded-lg px-4 py-1.5 m-2 w-full">
                     <input class="bg-transparent outline-none w-full text-gray-700 leading-tight focus:outline-none"
                         type="text" placeholder="Поиск">
                     <button class="flex-shrink-0 text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700 transition-colors duration-200"
                         type="button">
-                        <Icon name="ph:magnifying-glass" size="1.5em"></Icon>
+                        <Icon name="ph:magnifying-glass-light" size="1.5em"></Icon>
                     </button>
                 </div>
                 <nav class="flex items-center leading-5">
