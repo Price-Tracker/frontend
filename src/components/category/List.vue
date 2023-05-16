b021950869f9cb0ee0074e25edce743d9a2af865
ElXreno <elxreno@gmail.com>
2023-05-16 23:47:02 +0300

    Save space

--- src/components/category/List.vue
+++ src/components/category/List.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bg-white">
         <div class="container mx-auto px-4 py-8 sm:px-6 lg:px-8">
-            <p class="px-4 py-8 text-4xl  text-left font-bold text-gray-900 dark:text-white">Категории товаров</p>
+            <p class="px-4 py-8 pt-0 text-4xl text-left font-bold text-gray-900 dark:text-white">Категории товаров</p>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 <div v-for="category in categories" class="bg-white rounded-lg shadow-md overflow-hidden">
                     <CategoryItem :category="category" />
