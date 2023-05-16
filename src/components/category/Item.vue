4ccac7e8cef858f9711d6d2ca013ccf9a82af731
ElXreno <elxreno@gmail.com>
2023-05-17 00:00:04 +0300

    Remove image frow category, flex it

--- src/components/category/Item.vue
+++ src/components/category/Item.vue
@@ -1,8 +1,7 @@
 <template>
     <NuxtLink :to="'/category/' + category.id">
-        <img class="h-48 w-full object-cover" src="https://via.placeholder.com/350x200" alt="Category Image">
         <div class="p-4">
-            <h3 class="text-lg font-medium text-gray-900">{{ category.name }}</h3>
+            <h3 class="text-lg font-medium text-gray-900 text-center">{{ category.name }}</h3>
         </div>
     </NuxtLink>
 </template>
