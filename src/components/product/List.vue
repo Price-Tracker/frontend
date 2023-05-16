45dd321c0d503b97e74a5f380e933824425b1523
ElXreno <elxreno@gmail.com>
2023-05-16 18:58:05 +0300

    Tweak product alignment

--- src/components/product/List.vue
+++ src/components/product/List.vue
@@ -2,7 +2,7 @@
     <div class="bg-white">
         <div class="container mx-auto px-4 py-8 sm:px-6 lg:px-8">
             <p class="px-4 py-8 text-4xl text-center font-bold text-gray-900 dark:text-white">Хит продаж!</p>
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
                 <div v-for="product in products" class="bg-white shadow-md overflow-hidden">
                     <ProductItem :product="product" />
                 </div>
