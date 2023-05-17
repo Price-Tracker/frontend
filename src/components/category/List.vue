f2b447d53c969a11c4655e94afb531815b01a86f
ElXreno <elxreno@gmail.com>
2023-05-17 04:36:16 +0300

    Drop styling for dark theme

--- src/components/category/List.vue
+++ src/components/category/List.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bg-white">
         <div class="container mx-auto px-4 py-8 sm:px-6 lg:px-8">
-            <p class="px-4 py-8 pt-0 text-4xl text-left font-bold text-gray-900 dark:text-white">Категории товаров</p>
+            <p class="px-4 py-8 pt-0 text-4xl text-left font-bold text-gray-900">Категории товаров</p>
             <div class="flex flex-wrap flex-col md:flex-row gap-4">
                 <div v-for="category in categories" class="grow bg-white rounded-lg shadow-md overflow-hidden">
                     <CategoryItem :category="category" />
--- src/components/history/List.vue
+++ src/components/history/List.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bg-white">
         <div class="container mx-auto px-4 py-8 sm:px-6 lg:px-8">
-            <p class="px-4 py-8 text-4xl text-center font-bold text-gray-900 dark:text-white">История</p>
+            <p class="px-4 py-8 text-4xl text-center font-bold text-gray-900">История</p>
             <div class="grid grid-cols-1 gap-8">
                 <div v-for="product_in_history in history" class="bg-white shadow-md overflow-hidden">
                     <HistoryItem :product_in_history="product_in_history" />
