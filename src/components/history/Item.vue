85b860d0116d6cdc2b7ce76a461369e8f8fa531e
Neosqweuzzer <85544252+Neosqweuzzer@users.noreply.github.com>
2023-05-17 03:58:01 +0300

    History update

--- src/components/history/Item.vue
+++ src/components/history/Item.vue
@@ -1,21 +1,17 @@
 <template>
-    <!-- <div
-        class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-            src="https://via.placeholder.com/350x200" alt="">
-        <div class="flex flex-col justify-between p-4 leading-normal">
-            <p class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{{ product.name }}</p>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ history.date }}</p>
-        </div>
-    </div> -->
-
     <div class="mt-4 mb-4 flex gap-x-4">
         <img class="px-4 py-4 h-40 w-40 flex-none rounded-full" src="https://via.placeholder.com/350x200" alt="" />
-        <div class="min-w-0 flex-auto">
-          <p class="text-xl font-semibold leading-6 text-gray-900">{{ product_in_history.product }}</p>
-          <p class="mt-2 truncate text-lg leading-5 text-gray-500">{{ product_in_history.date}}</p>
+        <div class="place-self-center">
+            <p class="text-xl font-semibold leading-6 text-gray-900">{{ product_in_history.product }}</p>
+            <p class="text-left mt-2 truncate text-lg leading-5 text-gray-500">{{ product_in_history.date }}</p>
         </div>
-      </div>
+    </div>
+
+    <!-- <div class="bg-white rounded-lg shadow-lg p-4">
+        <img class="w-full" src="https://via.placeholder.com/350x200" alt="" />
+        <p class="mt-2 font-semibold leading-6 text-gray-900">{{ product_in_history.product }}</p>
+        <p class="mt-2 truncate leading-5 text-gray-500">{{ product_in_history.date }}</p>
+    </div> -->
 </template>
 
 <script>
--- src/components/history/List.vue
+++ src/components/history/List.vue
@@ -2,7 +2,6 @@
     <div class="bg-white">
         <div class="container mx-auto px-4 py-8 sm:px-6 lg:px-8">
             <p class="px-4 py-8 text-4xl text-center font-bold text-gray-900 dark:text-white">История</p>
-            <!-- <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"> -->
             <div class="grid grid-cols-1 gap-8">
                 <div v-for="product_in_history in history" class="bg-white shadow-md overflow-hidden">
                     <HistoryItem :product_in_history="product_in_history" />
