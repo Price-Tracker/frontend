733acf1fdbcec64f68daeced5337d879eff86bf3
Neosqweuzzer <85544252+Neosqweuzzer@users.noreply.github.com>
2023-05-23 01:28:03 +0300

    History update

--- src/components/history/Item.vue
+++ src/components/history/Item.vue
@@ -1,17 +1,16 @@
 <template>
-    <div class="mt-4 mb-4 flex gap-x-4">
-        <img class="px-4 py-4 h-40 w-40 flex-none rounded-full" src="https://via.placeholder.com/350x200" alt="" />
-        <div class="place-self-center">
-            <p class="text-xl font-semibold leading-6 text-gray-900">{{ product_in_history.product }}</p>
-            <p class="text-left mt-2 truncate text-lg leading-5 text-gray-500">{{ product_in_history.date }}</p>
+    <div class="relative">     
+        <div class="mt-4 mb-4 flex gap-x-4">
+            <img class="px-4 py-4 h-40 w-40 flex-none rounded-full" src="https://via.placeholder.com/350x200" alt="" />
+            <div class="place-self-center">
+                <p class="text-xl font-semibold leading-6 text-gray-900">{{ product_in_history.product }}</p>
+                <p class="text-left mt-2 truncate text-lg leading-5 text-gray-500">{{ product_in_history.date }}</p>
+            </div>
         </div>
+        <button class="absolute top-1 right-2 h-8 w-8 rounded-full hover:bg-gray-300 focus:outline-none">
+            <Icon name="ph:x" class="absolute top-0 right-0 h-8 w-8" size="1.5em"></Icon>
+        </button>
     </div>
-
-    <!-- <div class="bg-white rounded-lg shadow-lg p-4">
-        <img class="w-full" src="https://via.placeholder.com/350x200" alt="" />
-        <p class="mt-2 font-semibold leading-6 text-gray-900">{{ product_in_history.product }}</p>
-        <p class="mt-2 truncate leading-5 text-gray-500">{{ product_in_history.date }}</p>
-    </div> -->
 </template>
 
 <script>
--- src/components/product/ItemFull.vue
+++ src/components/product/ItemFull.vue
@@ -1,19 +1,59 @@
 <template>
-    <section class="text-gray-600 body-font overflow-hidden">
+    <div v-if="loading">
+        Загрузка...
+    </div>
+    <section v-else class="text-gray-600 body-font overflow-hidden">
+        <nav aria-label="breadcrumb" class="px-40 py-8 w-full p-4 bg-gray-50 text-gray-800">
+            <ol class="flex h-2 space-x-2">
+                <li class="flex items-center">
+                    <a rel="noopener noreferrer" href="#" title="Back to homepage" class="hover:underline">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                            class="w-5 h-5 pr-1 text-gray-600">
+                            <path
+                                d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z">
+                            </path>
+                        </svg>
+                    </a>
+                </li>
+                <li class="flex items-center space-x-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" fill="currentColor"
+                        class="w-2 h-2 mt-1 transform rotate-90 fill-current text-gray-400">
+                        <path d="M32 30.031h-32l16-28.061z"></path>
+                    </svg>
+                    <a rel="noopener noreferrer" href="#"
+                        class="flex items-center px-1 capitalize hover:underline">Parent</a>
+                </li>
+                <li class="flex items-center space-x-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" fill="currentColor"
+                        class="w-2 h-2 mt-1 transform rotate-90 fill-current text-gray-400">
+                        <path d="M32 30.031h-32l16-28.061z"></path>
+                    </svg>
+                    <a rel="noopener noreferrer" href="#"
+                        class="flex items-center px-1 capitalize hover:underline">Parent</a>
+                </li>
+                <li class="flex items-center space-x-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" fill="currentColor"
+                        class="w-2 h-2 mt-1 transform rotate-90 fill-current text-gray-400">
+                        <path d="M32 30.031h-32l16-28.061z"></path>
+                    </svg>
+                    <a rel="noopener noreferrer" href="#"
+                        class="flex items-center px-1 capitalize hover:underline hover:no-underline cursor-default">Current</a>
+                </li>
+            </ol>
+        </nav>
         <div class="container px-5 py-24 mx-auto">
             <div class="lg:w-4/5 mx-auto flex flex-wrap">
-                <div class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded">
+                <div class="flex flex-col items-center">
                     <img v-if="product.product.picture_url" :src="product.product.picture_url"
                         class="w-full max-w-sm hover:scale-105 transition duration-200">
                     <img v-else class="w-full max-w-sm"
                         src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==">
                 </div>
-
                 <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                    <div class="flex items-center">
+                    <div class="flex justify-between">
                         <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{{ product.product.name }}</h1>
-                        <Icon class="ml-3 hover:text-pink-600 transition-colors duration-200" name="ph:heart-light"
-                            size="2em" />
+                        <Icon class="hover:text-pink-600 transition-colors duration-200" name="ph:heart-light"
+                            size="2.5em" />
                     </div>
                     <div class="flex mb-4">
                         <span class="flex items-center">
@@ -54,7 +94,8 @@
                     <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
                     </div>
                     <div class="flex items-center">
-                        <div v-for="store in product.prices" class="title-font font-medium text-2xl text-gray-900">
+                        <div v-for="store in product.prices"
+                            class="title-font font-medium text-base md:text-xl lg:text-xl xl:text-2xl text-gray-900">
                             <span class="m-2">
                                 {{ store.price }} р.
                             </span>
@@ -97,6 +138,4 @@ onMounted(async () => {
         console.log(result.data)
     }
 })
-
-
 </script>
