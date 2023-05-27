93324c4af9d0e1f59c9899a6b63c35747768653f
ElXreno <elxreno@gmail.com>
2023-05-27 10:25:18 +0300

    Hotfix access token refresh on app startup

--- src/plugins/app-start.client.js
+++ src/plugins/app-start.client.js
@@ -2,8 +2,9 @@ export default defineNuxtPlugin(async nuxtApp => {
     console.log("App start");
 
     const authStore = useAuthStore();
+    const { $api } = nuxtApp;
 
     if (authStore.refreshToken) {
-        await authStore.refreshAccessToken();
+        await $api.user.refreshToken(authStore.refreshToken);
     }
 });
