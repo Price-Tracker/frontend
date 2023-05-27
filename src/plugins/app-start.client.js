90df6d6aa07438ee31bf06748f3ab1d87f33726e
ElXreno <elxreno@gmail.com>
2023-05-27 10:33:23 +0300

    Hotfix again

--- src/plugins/app-start.client.js
+++ src/plugins/app-start.client.js
@@ -2,9 +2,11 @@ export default defineNuxtPlugin(async nuxtApp => {
     console.log("App start");
 
     const authStore = useAuthStore();
-    const { $api } = nuxtApp;
+    const { $api } = useNuxtApp();
 
     if (authStore.refreshToken) {
-        await $api.user.refreshToken(authStore.refreshToken);
+        await $api.user.refreshToken(authStore.refreshToken)
+            .catch(() => authStore.resetAuth())
+            .then(result => result && authStore.saveUserTokens(result.data.access_token, result.data.refresh_token));
     }
 });
