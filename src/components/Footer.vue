27bcb226a43090b5ff4e0cb8392c7a2c979705c9
ElXreno <elxreno@gmail.com>
2023-06-08 10:33:13 +0000

    Temporary strip footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,11 @@
     <footer class="bg-black text-gray-300 p-4">
         <div class="container mx-auto px-4 py-4">
             <div class="grid gap-2 grid-cols-2 sm:grid-cols-3 justify-items-center md:auto-cols-min sm:max-h-32 sm:text-sm">
-                <div v-for="(item, index) in links"><a :href="item.to">{{ item.label }}</a> </div>
+                <div v-for="(item, index) in links">
+                    <NuxtLink :key="index" :to="item.to">
+                        {{ item.label }}
+                    </NuxtLink>
+                </div>
             </div>
         </div>
     </footer>
@@ -13,21 +17,7 @@ export default {
     data() {
         return {
             links: [
-                { label: 'О сервисе', to: '#' },
-                { label: 'Возможности', to: '#' },
-                { label: 'Применение', to: '#' },
-                { label: 'Партнерам', to: '#' },
-                { label: 'Цены', to: '#' },
-                { label: 'База знаний', to: '#' },
-                { label: 'Настройка системы', to: '#' },
-                { label: 'Справочный центр', to: '#' },
-                { label: 'Интеграции', to: '#' },
-                { label: 'Отчеты', to: '#' },
-                { label: 'Компания', to: '#' },
-                { label: 'Наша история', to: '#' },
-                { label: 'Преса о нас', to: '#' },
-                { label: 'Карьера', to: '#' },
-                { label: 'Контакты', to: '#' },
+                { label: 'О сервисе', to: '/about' },
             ],
         };
     },
